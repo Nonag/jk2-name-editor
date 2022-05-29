588ee1ad52bc90e534264cd1e5048bc968f165b1
Matthias Schott <matthias.schott@gmx.net>
2022-05-30 01:15:56 +0200

    feat: add legay color codes for shortened encoded colors

--- src/utils/hexColorToGameColor.ts
+++ src/utils/hexColorToGameColor.ts
@@ -1,3 +1,18 @@
+import chroma from 'chroma-js';
+
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+const legacyColors = {
+  black: { code: '0', hex: '000000' },
+  red: { code: '1', hex: 'FF0000' },
+  green: { code: '2', hex: '00FF00' },
+  yellow: { code: '3', hex: 'FFFF00' },
+  blue: { code: '4', hex: '0000FF' },
+  cyan: { code: '5', hex: '00FFFF' },
+  magenta: { code: '6', hex: 'FF00FF' },
+  white: { code: '7', hex: 'FFFFFF' },
+};
+/* eslint-enable sort-keys-fix/sort-keys-fix */
+
 /**
  * Hex color to jk2 hex color code.
  *
@@ -7,10 +22,17 @@
  */
 export const hexColorToGameColor = (hexColor: string, shortened = false) => {
   const hex = hexColor.replace('#', '');
+
   const hexShortened = hex.replace(/\w./g, (x: string) =>
     (((('0x' + x) as any) / 17 + 0.5) | 0).toString(16),
   );
 
+  // Get legacy color codes for hex colors that are close enough to one of the legacy colors.
+  const legacyCode = Object.values(legacyColors).find(({ code, hex }) => {
+    return chroma.deltaE(`#${hex}`, hexColor) < 5;
+  })?.code;
+
+  if (shortened && legacyCode) return `^${legacyCode}`;
   if (shortened && hexShortened.length === 3) return `^x${hexShortened}`;
   if (shortened && hexShortened.length === 4) return `^y${hexShortened}`;
   if (hex.length === 6) return `^X${hex}`;
